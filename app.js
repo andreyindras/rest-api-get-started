503ba0e0254c72c9c3aa26ac0430d1d768b87e2c
Rafael Labegalini <rafaellabegalini@vivaworks.com.br>
2025-05-07 16:43:37 -0300

    Implement basic GET endpoint to return 'Hello World!'

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
